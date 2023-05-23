c1b1ea05f9383c0ddfd0745d2aa591a03637cff4
Street <63686233+jstreet-swbno@users.noreply.github.com>
2023-05-22 20:57:39 -0500

    ...

--- MedioClinic/Extensions/AppBuilderExtensions.cs
+++ MedioClinic/Extensions/AppBuilderExtensions.cs
@@ -1,6 +1,24 @@
-﻿namespace MedioClinic.Extensions
+﻿using MedioClinic.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace MedioClinic.Extensions
 {
-    public class AppBuilderExtensions
+    public static class ApplicationBuilderExtensions
     {
+        /// <summary>
+        /// Attaches the culture recognition middleware to the request pipeline.
+        /// </summary>
+        /// <param name="builder">Application builder.</param>
+        /// <returns>Modified application builder.</returns>
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.UseMiddleware<CultureMiddleware>();
+        }
     }
 }
